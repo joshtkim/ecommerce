dbb8ede7413731cc09ceb826883ed74bc9895ca8
Joshua <jykim92@gmail.com>
2021-05-11 17:55:03 -0400

    cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/694bd8a5-794e-4909-b543-552397099fb1/zoomx-vaporfly-next-running-shoe-T5qg9m.png',},
-  { id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10', image: 'https://www.notebookcheck.net/uploads/tx_nbc2/air13teaser.jpg',},
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
 
   return(
@@ -18,7 +13,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
