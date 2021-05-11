24d065a52b614128082451509f297ffca7ba6fe5
Joshua <jykim92@gmail.com>
2021-05-11 13:47:19 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
