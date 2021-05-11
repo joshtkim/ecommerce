a4e94d23a65ac34bf5298098a112265160a5ee5d
Joshua <jykim92@gmail.com>
2021-05-11 16:07:10 -0400

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,13 +7,13 @@ import useStyles from './styles';
 
 const Navbar = () => {
   const classes = useStyles();
-  
+
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography variant="h6" className={classes.title} color="inherit">
-            <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+            {/* <img alt="Commerce.js" height="25px" className={classes.image} /> */}
             Commerce.js
           </Typography>
           <div className={classes.grow}/>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
   { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/694bd8a5-794e-4909-b543-552397099fb1/zoomx-vaporfly-next-running-shoe-T5qg9m.png',},
@@ -8,8 +10,11 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
+
   return(
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
